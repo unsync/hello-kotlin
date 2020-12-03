f285f0af6527fa9814402b349b60ac2a51cc6d7c
John <John>
2020-12-03 19:58:47 +0100

    [feat] update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Versions {
-	const val kotlin = "1.3.72"
-	const val junit = "5.6.2"
+	const val kotlin = "1.4.20"
+	const val junit = "5.7.0"
 }
 
 buildscript {
@@ -10,13 +10,17 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-        classpath("org.jetbrains.kotlin:kotlin-allopen:1.3.72")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:1.4.20")
     }
 }
 
 plugins {
-	kotlin("jvm") version "1.3.72"
+	kotlin("jvm").version("1.4.20")
+	kotlin("plugin.allopen").version("1.4.20")
+	id("com.github.ben-manes.versions").version("0.36.0")
+	id("io.gitlab.arturbosch.detekt").version("1.14.2")
+	id("org.jlleitschuh.gradle.ktlint").version("9.4.1")
 }
 
 group = "kotlin.hello"
@@ -41,7 +45,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
 
-    implementation("org.apache.commons:commons-lang3:3.9")
+    implementation("org.apache.commons:commons-lang3:3.11")
 
 	testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
